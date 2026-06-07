c5dc681bcd4814db3b2342b7799c3e5db6fd321d
Areeba Waqar <areebawaqar007@gmail.com>
2026-06-08 04:28:04 +0500

    signup functioality added

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
+import cors from 'cors';
 
-//components
+// components
 import Connection from './database/db.js';
-// import Router from './routes/route.js';
-
+import Router from './routes/route.js';
 
 dotenv.config();
 
 const app = express();
 
-// app.use(cors());
-// app.use(bodyParser.json({ extended: true }));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use('/', Router);
+// middleware
+app.use(cors());
+app.use(express.json()); // ✅ IMPORTANT
+app.use(express.urlencoded({ extended: true })); // optional
 
+// routes
+app.use('/', Router);
 
 const PORT = 8000;
 
-Connection()
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+Connection();
+
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on PORT ${PORT}`)
+);
