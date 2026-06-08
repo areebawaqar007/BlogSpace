b009cee001ecb5e6f1e8f8a554e5e53c7f01ff1e
Areeba Waqar <areebawaqar007@gmail.com>
2026-06-08 08:42:21 +0500

    fix: properly track client folder files

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+
+import Login from "./components/account/Login";
+import DataProvider from "./context/DataProvider";
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
+import { useState } from "react";
+import { Box } from "@mui/material";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+
+const PrivateRoute = ({ isAuthenticated, ...props }) => {
+  const token = sessionStorage.getItem("accessToken");
+  return isAuthenticated && token ? (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  ) : (
+    <Navigate replace to="/account" />
+  );
+};
+
+function App() {
+  const [isAuthenticated, isUserAuthenticated] = useState(false);
+
+  return (
+    <DataProvider>
+      <BrowserRouter>
+        <Header />
+        <div style={{ marginTop: 64 }}>
+          <Routes>
+            <Route
+              path="/account"
+              element={<Login isUserAuthenticated={isUserAuthenticated} />}
+            />
+
+            <Route
+              path="/"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/" element={<Home />} />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </DataProvider>
+  );
+}
+
+export default App;
